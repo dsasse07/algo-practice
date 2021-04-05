f6c5d6847907458ed6a1326f49505648291e7aee
Daniel Sasse <dsasse07@gmail.com>
2021-04-05 18:05:28 -0400

    Leetcode 151 String reversal done

--- reverse-string-leetcode-151/javascript/solution.js
+++ reverse-string-leetcode-151/javascript/solution.js
@@ -3,3 +3,11 @@ const s2 = "  hello world  "
 const s3 = "a good   example"
 const s4 = "  Bob    Loves  Alice   "
 const s5 = "Alice does not even like bob"
+
+function reverseString(string){
+  const trimmedString = string.trim()
+  const pattern = /\s{2,}/g
+  const normalizedString = trimmedString.replace(pattern, " ")
+  return normalizedString.split(" ").reverse().join(" ")
+}
+
