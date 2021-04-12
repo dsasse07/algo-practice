94f4bb7ea39e008ab32dbce24140326b3d2bb9f7
Daniel Sasse <dsasse07@gmail.com>
2021-04-12 14:38:40 -0400

    HR hashes sherlock-and-anagrams

--- hackerRank/interviewPrep/hashes/sherlock-and-anagrams/javascript/solution.js
+++ hackerRank/interviewPrep/hashes/sherlock-and-anagrams/javascript/solution.js
@@ -1,5 +1,33 @@
-function solution (  ){
+function solution ( string ){
+  const substrings = {}
+  let counter = 0
+
+  // iterate through each letter as a starting point
+  for( let i = 0; i < s.length; i++){
+    // create a substring starting at the letter above, and extending 1 letter incrementall
+    // until the end of the string is reached
+    for (let j = 1; i+j <= s.length; j++){
+      //sort the substrings alphabetically, so that anagrams will stack
+      const sub = s.substr(i, j).split("").sort().join("")
+      if (!sub) continue
+      // store the substring into a hash. If if previously was stored,
+      // we have a matching pair! 
+      // Increase the pair counter by the number of stacked versions.
+      // Incrementing counter in this way prevents having to use
+      // factorial afterwards to get combination count.
+
+      // After dealing with the counter, increment its stack count
+      if ( substrings[sub] ) {
+        counter += substrings[sub]
+        substrings[ sub ] += 1
+      } else {
+        // If the substring wasn't previously stored, add it to hash
+        substrings[ sub ] = 1
+      }
+    }
+  }
   
+  return counter
 } 
 
 module.exports = { solution }
--- hackerRank/interviewPrep/hashes/sherlock-and-anagrams/javascript/solution.test.js
+++ hackerRank/interviewPrep/hashes/sherlock-and-anagrams/javascript/solution.test.js
@@ -1,13 +1,40 @@
 const { solution } = require('./solution')
 
+const test1 = "loom"
+const test2 = "mom"
+const test3 = "noon"
+const test4 = "dump"
+const test5 = "ifailuhkqq"
+const test6 = "kkkk"
+const test7 = "cdcd"
 
 
 describe('solution function', () => {
-  test('should ', () => {
-    
-
-    expect( solution(  ) ).toEqual(  )
+  test('loom has 1 anagram pair', () => {
+    // [o,o]
+    expect( solution( test1 ) ).toEqual( 1 )
   })
-  
+  test('mom has 2 anagram pairs', () => {
+    // [m,m], [mo, om]
+    expect( solution( test2 ) ).toEqual( 2 )
+  })
+  test('noon has 2 anagram pairs', () => {
+    // [n,n], [o,o], [no, on], [noo, oon]
+    expect( solution( test3 ) ).toEqual( 4 )
+  })
+  test('dump has 0 anagram pairs', () => {
+    expect( solution( test4 ) ).toEqual( 0 )
+  })
+  test('ifailuhkqq has 3 anagram pairs', () => {
+    // [i,i], [q,q], [ifa, fai]
+    expect( solution( test5 ) ).toEqual( 3 )
+  })
+  test('kkkk has 10 anagram pairs', () => {
+    expect( solution( test6 ) ).toEqual( 10 )
+  })
+  test('cdcd has 5 anagram pairs', () => {
+    // [c,c], [d,d], [cd, cd], [cd,cd], [dc,cd] 
+    expect( solution( test7 ) ).toEqual( 5 )
+  })
 })
 
