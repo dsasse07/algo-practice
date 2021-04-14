53701ca42f5c86f4fb05c2246462874aee7dc4cb
Daniel Sasse <dsasse07@gmail.com>
2021-04-14 10:07:18 -0400

    implemented quicksort in Ruby

--- hackerRank/interviewPrep/sorting/quicksort/ruby/solution.rb
+++ hackerRank/interviewPrep/sorting/quicksort/ruby/solution.rb
@@ -1,6 +1,33 @@
-
+require 'pry'
 class Solution
-  def solve(   )
+  def solve( array )
+    quicksort( array, 0, array.length - 1 )
+  end
 
+  private
+
+  def quicksort( array, left_index, right_index )
+    return if left_index >= right_index
+    pivot_index = ( left_index + right_index ) / 2
+    pivot = array[ pivot_index ]
+    split_index = swap_and_split( array, left_index, right_index, pivot)
+    quicksort( array, left_index, split_index - 1)
+    quicksort( array, split_index, right_index)
+
+    array
+  end
+
+  def swap_and_split( array, left_index, right_index, pivot )
+    while left_index <= right_index do
+      left_index += 1 while array[left_index] < pivot
+      right_index -= 1 while array[right_index] > pivot
+      break if left_index > right_index
+
+      array[left_index], array[right_index] = array[right_index], array[left_index]
+      left_index += 1
+      right_index -= 1
+    end
+
+    left_index
   end
 end
--- hackerRank/interviewPrep/sorting/quicksort/ruby/spec/solution_spec.rb
+++ hackerRank/interviewPrep/sorting/quicksort/ruby/spec/solution_spec.rb
@@ -1,11 +1,11 @@
-require './solution.rb'
+require_relative '../solution.rb'
 
-
+test1 = [5,1,1,19,3,9,6,1,8]
 
 describe "solution" do 
   it "should " do
     solution = Solution.new
-    expect( solution.solve( ) ).to eql( )
+    expect( solution.solve( test1 ) ).to eql( [1,1,1,3,5,6,8,9,19] )
   end
 
 end
