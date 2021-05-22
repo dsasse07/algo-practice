bdd8ccccc8ca7070c0971c4f03e84c9fb10de83b
Daniel Sasse <dsasse07@gmail.com>
2021-05-22 17:10:12 -0400

    LC 240 search matrix

---
+++ leetcode/0240-search-2d-matrix-2/javascript/solution.js
@@ -0,0 +1,52 @@
+/*
+Write an efficient algorithm that searches for a target value in an m x n integer matrix. The matrix has the following properties:
+
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+
+
+Example 1:
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+Output: true
+
+Example 2:
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20
+Output: false
+
+Constraints:
+m == matrix.length
+n == matrix[i].length
+1 <= n, m <= 300
+-109 <= matix[i][j] <= 109
+All the integers in each row are sorted in ascending order.
+All the integers in each column are sorted in ascending order.
+-109 <= target <= 109
+*/
+
+const t1 = [ [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], 5]
+const t2 = [ [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], 20]
+
+
+
+function searchMatrix([matrix, target]){
+    // Start at top right corner or bottom left corners to that values only grow
+    // In one direcion
+    let [cRow, cCol] = [0, matrix[0].length]
+    
+    while ( cRow < matrix.length && cCol >= 0){
+        if (target === matrix[cRow][cCol]) return true
+        if ( target > matrix[cRow][cCol] ){
+            cRow++ 
+        } else {
+            cCol--
+        }
+    }
+    
+    return false
+    
+}
+
+
+console.log(searchMatrix(t1))
+console.log(searchMatrix(t2))
+
