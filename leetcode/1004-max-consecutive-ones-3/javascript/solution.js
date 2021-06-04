72d8559e5fadf51c86c83c422b0a310a248f1fe0
Daniel Sasse <dsasse07@gmail.com>
2021-06-03 21:55:39 -0400

    1004 max consecutive ones 3

---
+++ leetcode/1004-max-consecutive-ones-3/javascript/solution.js
@@ -0,0 +1,30 @@
+const t1 = [1,1,1,0,0,0,1,1,1,1,0]
+const k1 = 2
+
+function findLength(nums, k){
+  let zeros = 0
+  let start = 0
+  let end = 0
+  let length = 0
+
+  while (end < nums.length){
+    if (nums[end] === 0){
+      zeros++
+      while (zeros > k){
+        if(nums[start] === 0){
+          zeros--
+        } 
+        start++
+      }
+    }
+    length = Math.max(length, end - start + 1)
+    end++
+  }
+  return length
+  
+}
+
+
+console.log(findLength(t1,k1))
+
+
