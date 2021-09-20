c48ddb38df981b9d90ed413a01c72f110b63dbab
Daniel Sasse <dsasse07@gmail.com>
2021-09-20 19:21:05 -0400

    1721 swapping LL nodes

---
+++ leetcode/1721-swapping-nodes-linked-list/solution.js
@@ -0,0 +1,58 @@
+/**
+ * You are given the head of a linked list, and an integer k.
+
+Return the head of the linked list after swapping the values of the kth node from the beginning and the kth node from the end (the list is 1-indexed).
+
+Example 1:
+
+Input: head = [1,2,3,4,5], k = 2
+Output: [1,4,3,2,5]
+
+Example 2:
+Input: head = [7,9,6,6,7,8,3,0,9,5], k = 5
+Output: [7,9,6,6,8,7,3,0,9,5]
+
+Example 3:
+Input: head = [1], k = 1
+Output: [1]
+
+Example 4:
+Input: head = [1,2], k = 1
+Output: [2,1]
+
+Example 5:
+Input: head = [1,2,3], k = 2
+Output: [1,2,3]
+
+Constraints:
+
+The number of nodes in the list is n.
+1 <= k <= n <= 105
+0 <= Node.val <= 100
+ */
+
+function swapNodes(head, k) {
+  let firstPass = head,
+    secondPass = head,
+    firstNode,
+    temp
+
+  for (let i = 1; i < k; i++) {
+    firstPass = firstPass.next
+  }
+  firstNode = firstPass
+  firstPass = firstPass.next
+
+  // The first pass will reach will end while
+  // secondPass has 'k' nodes remaining
+  while (firstPass) {
+    firstPass = firstPass.next
+    secondPass = secondPass.next
+  }
+
+  temp = firstNode.val
+  firstNode.val = secondPass.val
+  secondPass.val = temp
+
+  return head
+}
